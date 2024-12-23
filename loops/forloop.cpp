3b95a32fe2e819c947c7b66319d979ff378569e7
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2024-12-23 23:09:09 +0530

    loops

---
+++ loops/forloop.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+for ( int i=100; i > 0; i--){
+    cout << "stark " << i << endl;
+}
+    return 0;
+}
---
+++ loops/whileloop.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int i = 3;
+    while(i<=5) {
+        cout << "stark" << i << endl;
+        i++;
+    }
+    return 0;
+}
