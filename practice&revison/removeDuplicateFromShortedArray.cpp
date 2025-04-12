879ec246cfa1e42ee8d81e42014fb1ab7f64f0d0
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2025-04-12 23:38:26 +0530

    push

---
+++ practice&revison/removeDuplicateFromShortedArray.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int i = 0; // slow pointer
+
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+    int k = solution.removeDuplicates(nums);
+
+    cout << "Number of unique elements: " << k << endl;
+    cout << "Modified array: ";
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
